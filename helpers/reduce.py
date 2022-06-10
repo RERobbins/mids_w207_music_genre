2b36a4ac031081040b87d0fa5aba976377a18f0c
Richard E. Robbins <RERobbins@iTinker.net>
2022-06-10 14:53:01 -0500

    Add documentation to reduce.py and split.py

--- helpers/reduce.py
+++ helpers/reduce.py
@@ -9,6 +9,42 @@ def load_and_distill(
     labels=BASE_GENRES, multi_label=False, features=BASE_FEATURES, tags=[], pickle=[]
 ):
 
+    """
+    Load MTG_Jamendo reference data and reduce it to a working dataset.
+    
+    Load the sharded MTG_Jamendo reference data and retain elements according 
+    to the paramters described below.  Return the result as a pandas DataFrame 
+    and if the pickle parameter is specified, save the result as a pickle file, 
+    inferring compression from the file name supplied.
+
+    Parameters
+    ----------
+    labels: a list of strings
+        The labels (genres) for included tracks, the default is helpers.constants.BASE_GENRES.
+
+    multi_label: a boolean
+        If True, a track will be included if it associated with at least one label from the 
+        label paramters, otherwise only tracks associated with a single label but not more 
+        than one will be included.  The default is False.
+
+    features: a list of strings
+        The features to be included for each track, the default is helpers.constants.BASE_FEATURES.
+
+    tags: 'all' or a list of strings
+        The tags to be included for each track.  If the paramter is the empty list (the default), 
+        tags will not be included.  If the paramter is 'all', all tags will be included.
+
+    pickle: a string
+        If a string is supplied, it will be used as the name of the pickle file for the result.  
+        Compression is inferred from the name.  If the paramter is not specified, the pickle 
+        file will not be created.
+
+    Returns
+    -------
+    pandas DataFrame
+        The DataFrame specified by the parameters.    
+    """
+
     data = ReadFilesIntoDataframe().read_mtg_jamendo_files()
 
     if multi_label:
--- helpers/split.py
+++ helpers/split.py
@@ -3,10 +3,29 @@ import sys
 import numpy as np
 import pandas as pd
 
-from helpers.reduce import load_and_distill
-
 
 def tag_label_feature_split(df):
+    """
+    Returns the tags, labels and features from an MTG_Jamendo working dataset.
+    
+    A working MTG_Jamendo dataset contains three clusters of fields.  Metadata tags are 
+    reflected in columns with names starting with "tag", labels are reflected in columns
+    with names starting with "genre", and features are contained in all other columns.
+
+    This function takes an MTG_Jamendo dataset and splits it into three pandas DataFrames, for
+    the tags, labels and features present in the dataset, respectively.
+
+    Parameters
+    ----------
+    df: a pandas DataFrame
+        A working MTG_Jamendo dataset, typically created by helpers.reduce.load_and_distill().
+
+    Returns
+    -------
+    three pandas DataFrames
+        tag DataFrame, label DataFrame, features DataFrame    
+    """
+
     tags = []
     labels = []
     features = []
