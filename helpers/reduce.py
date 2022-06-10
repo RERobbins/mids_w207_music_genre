7556f0948801b78f2b75e79d98984b43d47f7fc6
Richard E. Robbins <RERobbins@iTinker.net>
2022-06-10 12:12:58 -0500

    Update helpers. . .

--- helpers/reduce.py
+++ helpers/reduce.py
@@ -2,36 +2,26 @@ import numpy as np
 import pandas as pd
 
 from helpers.stitch import ReadFilesIntoDataframe
+from helpers.constants import BASE_GENRES, BASE_FEATURES
 
 
-def load_and_distill(multi_label=False, tags=True):
+def load_and_distill(
+    labels=BASE_GENRES, multi_label=False, features=BASE_FEATURES, tags=[], pickle=[]
+):
 
     data = ReadFilesIntoDataframe().read_mtg_jamendo_files()
-
-    labels = [
-        "genre_blues",
-        "genre_classical",
-        "genre_country",
-        "genre_disco",
-        "genre_hiphop",
-        "genre_jazz",
-        "genre_metal",
-        "genre_pop",
-        "genre_reggae",
-        "genre_rock",
-    ]
 
     if multi_label:
         result = data.drop(data[data[labels].sum(axis=1) == 0].index)
     else:
         result = data.drop(data[data[labels].sum(axis=1) != 1].index)
-    discard_columns = [
-        column
-        for column in result.columns
-        if (
-            (column.startswith("genre") and column not in labels)
-            or (not tags and column.startswith("tag"))
-        )
-    ]
-    result.drop(discard_columns, axis=1, inplace=True)
+
+    if tags == "all":
+        tags = result.columns[result.columns.str.startswith("tag")].tolist()
+
+    result = result[tags + features + labels]
+
+    if pickle:
+        result.to_pickle(path=pickle, compression="infer")
+
     return result
