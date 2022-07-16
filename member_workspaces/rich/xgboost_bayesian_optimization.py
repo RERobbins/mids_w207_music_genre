a7dfec8c53fc0167790ce89f25cc40afac571515
Richard Robbins <RERobbins@iTinker.net>
2022-07-16 11:11:41 -0500

    bayesian optimization, switch to pipelines to address resampling data leakage issue

--- member_workspaces/rich/xgboost_bayesian_optimization.py
+++ member_workspaces/rich/xgboost_bayesian_optimization.py
@@ -7,7 +7,6 @@ import numpy as np
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.model_selection import cross_val_score, cross_validate
@@ -15,6 +14,7 @@ from sklearn.model_selection import cross_val_score, cross_validate
 from xgboost import XGBClassifier
 from bayes_opt import BayesianOptimization
 
+from imblearn.pipeline import Pipeline
 from imblearn.combine import SMOTEENN, SMOTETomek
 from imblearn.over_sampling import SMOTE
 from imblearn.under_sampling import EditedNearestNeighbours
@@ -28,14 +28,9 @@ DATASET_FOLDER = "../../datasets/"
 
 experiment_parameters = {}
 
-def get_data(dataset, scaling=False, pca_components=None, resampling=None):
-    
-    # set the global parameter tags for this exercise
-    experiment_parameters['dataset'] = dataset
-    experiment_parameters['scaled'] = 'yes' if scaling else 'no'
-    experiment_parameters['pca_components'] = 0 if pca_components is None else pca_components
-    experiment_parameters['resampling'] = "none" if resampling is None else resampling
-    
+
+def get_data(dataset):
+
     # read a dataset
     df = pd.read_pickle(DATASET_FOLDER + dataset)
 
@@ -47,44 +42,15 @@ def get_data(dataset, scaling=False, pca_components=None, resampling=None):
         X, y, test_size=0.2, random_state=1962, shuffle=True, stratify=y
     )
 
-    # use scaling or pca as required
-    pipe=None
-    
-    if scaling and pca_components is not None:
-        pipe = Pipeline([('scaler', StandardScaler()),
-                         ('pca', PCA(random_state=1962, n_components=pca_components))])
-        
-    if scaling and pca_components is None:
-        pipe = Pipeline([('scaler', StandardScaler())])
-        
-    if not scaling and pca_components is not None:
-        pipe = Pipeline([('pca', PCA(random_state=1962, n_components=pca_components))])
-          
-    if pipe is not None:
-        X_train = pipe.fit_transform(X_train)    
-
-    # resample as required
-    smt = None
-    
-    if resampling == 'SMOTE':
-        smt = SMOTE (random_state=1962, n_jobs=-1)
-        
-    if resampling == 'SMOTEENN':
-        enn = EditedNearestNeighbours(kind_sel='mode', n_neighbors=3, n_jobs=-1)
-        smt = SMOTEENN (random_state=1962, enn=enn)
-
-    if resampling == "SMOTETomek":
-        smt = SMOTETomek(random_state=1962, n_jobs=-1)
-        
-    if smt is not None:
-        X_train, y_train = smt.fit_resample(X_train, y_train)
-        
     return X_train, y_train
 
 
 def xgboost_cv(
     features,
     labels,
+    scaling,
+    pca_components,
+    resampling,
     learning_rate,
     n_estimators,
     max_depth,
@@ -96,8 +62,36 @@ def xgboost_cv(
     arguments = locals()
     arguments.pop("features")
     arguments.pop("labels")
+    arguments.pop("scaling")
+    arguments.pop("pca_components")
+    arguments.pop("resampling")
 
-    estimator = XGBClassifier(
+    # set the global parameter tags for this exercise
+    experiment_parameters["scaled"] = "yes" if scaling else "no"
+    experiment_parameters["pca_components"] = (
+        0 if pca_components is None else pca_components
+    )
+    experiment_parameters["resampling"] = "none" if resampling is None else resampling
+
+    steps = []
+
+    if scaling:
+        steps.append(("scaling", StandardScaler()))
+
+    if pca_components is not None:
+        steps.append(("pca", PCA(random_state=1962, n_components=pca_components)))
+
+    if resampling == "SMOTE":
+        steps.append(("resampling", SMOTE(random_state=1962, n_jobs=-1)))
+
+    if resampling == "SMOTEENN":
+        enn = EditedNearestNeighbours(kind_sel="mode", n_neighbors=3, n_jobs=-1)
+        steps.append(("resampling", SMOTEENN(random_state=1962, enn=enn)))
+
+    if resampling == "SMOTETomek":
+        steps.apend(("resampling", SMOTETomek(random_state=1962, n_jobs=-1)))
+
+    classifier = XGBClassifier(
         learning_rate=learning_rate,
         n_estimators=n_estimators,
         max_depth=max_depth,
@@ -111,12 +105,16 @@ def xgboost_cv(
         seed=seed,
     )
 
+    steps.append(("classifier", classifier))
+
+    model = Pipeline(steps)
+
     scoring = ["matthews_corrcoef", "f1_macro", "accuracy"]
 
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
         scores = cross_validate(
-            estimator, features, labels, scoring=scoring, cv=10, error_score=0, n_jobs=-1
+            model, features, labels, scoring=scoring, cv=10, error_score=0, n_jobs=-1
         )
 
     score_summary = {key.strip("test_"): value.mean() for key, value in scores.items()}
@@ -126,15 +124,19 @@ def xgboost_cv(
         outfile.write("\n")
 
     result = scores["test_matthews_corrcoef"].mean()
-    
+
     return result
 
-def optimize_xgboost(features, labels):
+
+def optimize_xgboost(features, labels, scaling, pca_components, resampling):
     def xgboost_crossval(learning_rate, n_estimators, max_depth, gamma, reg_alpha):
 
         return xgboost_cv(
             features=features,
             labels=labels,
+            scaling=scaling,
+            pca_components=pca_components,
+            resampling=resampling,
             learning_rate=learning_rate,
             n_estimators=int(n_estimators),
             max_depth=int(max_depth),
@@ -160,22 +162,25 @@ def optimize_xgboost(features, labels):
 
 
 if __name__ == "__main__":
-    
-    dataset='dataset_01_mean.pickle'
-       
-    experiment_parameters['model'] = "XGBoostClassifier"
-    experiment_parameters['timestamp'] = int(time.time())
+
+    dataset = "dataset_01_mean.pickle"
+
+    experiment_parameters["model"] = "XGBoostClassifier"
+    experiment_parameters["dataset"] = dataset
+    experiment_parameters["timestamp"] = int(time.time())
 
     arg_scaling = [False, True]
-    arg_pca = [None, .95]
-    arg_resampling = [None, 'SMOTE', 'SMOTEENN', 'SMOTETOMEK']
-    arg_dicts = [{'scaling': scaling, 'pca_components': pca_components, 'resampling': resampling}
-                 for scaling in arg_scaling
-                 for pca_components in arg_pca
-                 for resampling in arg_resampling]
+    arg_pca = [None, 0.95]
+    arg_resampling = [None, "SMOTE", "SMOTEENN", "SMOTETOMEK"]
+    arg_dicts = [
+        {"scaling": scaling, "pca_components": pca_components, "resampling": resampling}
+        for scaling in arg_scaling
+        for pca_components in arg_pca
+        for resampling in arg_resampling
+    ]
 
     for arg_dict in arg_dicts:
         print("--- Optimizing XGBoost ---")
-        print (f"Dataset: {dataset} Arguments: {arg_dict}")
-        features, labels = get_data(dataset, **arg_dict)
-        optimize_xgboost(features, labels)
+        print(f"Dataset: {dataset} Arguments: {arg_dict}")
+        features, labels = get_data(dataset)
+        optimize_xgboost(features, labels, **arg_dict)
